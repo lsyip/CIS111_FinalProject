02af30a1f46fc75f9d7d000aed65c6ccc0cc9d4e
lsyip <lsyip62@gmail.com>
2020-12-09 16:27:05 -0500

    Javadoc comments added

--- src/com/company/UserAccount.java
+++ src/com/company/UserAccount.java
@@ -3,16 +3,20 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * The UserAccount class contains the variables and methods necessary to manage several created user accounts at one time
+ * and present data about each individual account through the main UserAccountUI class.
+ */
 public class UserAccount{
 
     //Scanner object to collect user input
     Scanner keyboard = new Scanner(System.in);
 
-    //ArrayList containing: this will store all usernames and passwords in separate lists
+    //ArrayList containing all usernames and passwords in separate lists
     ArrayList<String> username = new ArrayList<>();
     ArrayList<String> password = new ArrayList<>();
 
-    //ArrayList spot for totalBalance
+    //ArrayList for totalBalance
     ArrayList<Double> totalBalance = new ArrayList<>();
 
     //2D ArrayList for transactionHistory
@@ -21,32 +25,49 @@ public class UserAccount{
 
 
     //Variables
-    String user;
-    String pass;
+    String user; //Username
+    String pass; //Password
     double depositAmount;
     double withdrawAmount;
     double userBalance;
 
-
+    /**
+     * This is the class constructor for the UserAccount class.
+     * The constructor is called when an object of the UserAccount class is created.
+     */
     public UserAccount(){
 
     }
 
-    public void getUsers(String un, String p){
-        user = un;
-        pass = p;
+    /**
+     * This is the class getter for the UserAccount class.
+     * @param username the username created by the user
+     * @param password the password created by the user
+     */
+    public void getUsers(String username, String password){
+        user = username;
+        pass = password;
     }
 
+    /**
+     * The checkNewUserAccount() method verifies that a newly created user account is unique and
+     * does not match any existing accounts.
+     * @return true if the user account already exists
+     */
     // This checks if an user already exists
     public boolean checkNewUserAccount(){
         for(int i = 0; i < username.size(); i++){
-            if(user.equalsIgnoreCase(username.get(i)) || pass.equalsIgnoreCase(password.get(i))){
+            if(user.equals(username.get(i))){
                 return true;
             }
         }
         return false;
     }
 
+    /**
+     * The setUserAccount() method adds information to the username, password, totalBalance, and transactionHistory ArrayLists
+     * when a new account is created.
+     */
     public void setUserAccount(){
         //adding to an arrayList is simply .add()
         username.add(user);
@@ -61,6 +82,13 @@ public class UserAccount{
         historyIndex++;
     }
 
+    //Checks for matching username and password
+    //Used to interact with the correct user account
+
+    /**
+     * The checkUserAccount() method verifies that the entered username and password match.
+     * @return true if user account information is a match
+     */
     public boolean checkUserAccount(){
         for(int i = 0; i < username.size(); i++){
             if(user.equalsIgnoreCase(username.get(i)) && pass.equals(password.get(i))){
@@ -70,76 +98,93 @@ public class UserAccount{
         return false;
     }
 
-    // When doing the deposit and withdraw method and then setting the total balance, make sure the total balance corresponds to the correct account ->
-    // Relate to the if statement in the checkBalance method and you'll end up using the .set() method rather than the .add() since you're changing the value already there
-    // .set(i, depositValue or withdrawValue) -> this'll correspond to whatever account is active.
-    // totalBalance.get(i) will retrieve the value corresponding to the account
-
-    //deposit method here
+    /**
+     * Prompts the user to enter a dollar amount to deposit, updates the user's total balance, and displays the new balance after the transaction.
+     */
     public void depositMethod(){
         for (int i = 0; i < totalBalance.size(); i++) {
+            //Find the correct user index and retrieve their total account balance
             if (user.equalsIgnoreCase(username.get(i)) && pass.equals(password.get(i))) {
                 userBalance = (totalBalance.get(i));
                 System.out.printf(user.substring(0,1).toUpperCase() + user.substring(1) + " your current balance is: $%.2f \n", userBalance);
+
+                //Prompt user for deposit amount
                 System.out.println("Enter deposit amount: ");
                 depositAmount = keyboard.nextDouble();
 
+                //Check if deposit amount is a positive number
                 while (depositAmount < 0) {
                     System.out.println("Enter deposit amount: ");
                     depositAmount = keyboard.nextDouble();
                 }
 
+                //Update totalBalance value
                 double newBalance = userBalance + depositAmount;
                 totalBalance.set(i, newBalance);
+
+                //Add deposit amount to transaction history array
                 transactionHistory.get(i).add("You deposited $" + depositAmount);
+
+                //Display new total balance
                 System.out.printf(user.substring(0,1).toUpperCase() + user.substring(1) + " your new balance is: $%.2f \n", newBalance);
                 break; //no point in continuing the loop
             }
         }
     }
 
-
-    //withdraw method here
+    /**
+     * Prompts the user to enter a dollar amount to withdraw, updates the user's total balance, and displays the total balance after the transaction.
+     */
     public void withdrawMethod(){
-            //Find the user account and access totalBalance
+            //Find the correct user account index and retrieve their totalBalance
             for (int i = 0; i < totalBalance.size(); i++) {
                 if (user.equalsIgnoreCase(username.get(i)) && pass.equals(password.get(i))) {
                     userBalance = totalBalance.get(i);
                     System.out.printf(user.substring(0,1).toUpperCase() + user.substring(1) + " your current balance is: $%.2f \n", userBalance);
+
+                    //Prompt user for withdrawal amount
                     System.out.println("Enter withdraw amount: ");
                     withdrawAmount = keyboard.nextDouble();
 
-                    //Checks for negative number and insufficient funds
+                    //Checks for negative withdrawal amount or insufficient funds
                     while (withdrawAmount < 0 || withdrawAmount > userBalance) {
                         System.out.println("Invalid withdrawal amount." + "\n" + "Enter withdraw amount: ");
                         withdrawAmount = keyboard.nextDouble();
                     }
 
-                    //Update user's totalBalance and print information
+                    //Update user's totalBalance
                     double newBalance = userBalance - withdrawAmount;
                     totalBalance.set(i, newBalance);
+
+                    //Add withdraw action to transaction history
                     transactionHistory.get(i).add("You withdrew $" + withdrawAmount);
+
+                    //Display new user balance
                     System.out.printf(user.substring(0,1).toUpperCase() + user.substring(1) + " your new balance is: $%.2f \n", newBalance);
                     break;
                 }
             }
         }
 
-    // The transactionHistory method displays all user transaction history for a SINGLE user
+    /**
+     * Displays all user transaction history for a single user.
+     */
     public void transactionHistory(){
         for (int i = 0; i < totalBalance.size(); i++) {
+            //Find the index of the current user and print everything from that array
             if (user.equalsIgnoreCase(username.get(i)) && pass.equals(password.get(i)))
                 for (String n : transactionHistory.get(i)){
                     System.out.println(n);
                 }
             }
-
     }
 
-    //This method allows for the correct balance to be shown for the corresponding account
-    //totalBalance.get(0) == username.get(0) and so on
+    /**
+     * The checkBalance() method retrieves the appropriate user balance depending on which user is currently logged in.
+     */
     public void checkBalance(){
         for(int i = 0; i < totalBalance.size(); i++){
+            //Find the index of that user and print the balance stored there
             if(user.equalsIgnoreCase(username.get(i)) && pass.equals(password.get(i))){
                 userBalance = totalBalance.get(i);
                 System.out.printf(user.substring(0,1).toUpperCase() + user.substring(1) + " your current balance is: $%.2f \n", userBalance);
@@ -147,5 +192,4 @@ public class UserAccount{
             }
         }
     }
-
 }
--- src/com/company/UserAccountUI.java
+++ src/com/company/UserAccountUI.java
@@ -2,12 +2,23 @@ package com.company;
 
 import java.util.Scanner;
 
+/**
+ * The UserAccountUI class contains the main User Interface method for a basic banking application.
+ */
 public class UserAccountUI {
 
+    /**
+     * The main method presents the user interface for a basic banking application.
+     * The first menu asks the user to create a new account or to sign in, for returning users.
+     * The second menu presents banking options including deposit, withdraw, check balance, and show transaction history
+     * @param args The command line arguments.
+     */
     public static void main(String[] args) {
 
-        //Classes
+        //Creates a UserAccount object for interacting with user accounts
         UserAccount useraccount = new com.company.UserAccount();
+
+        //Create a Scanner object for keyboard input
         Scanner kbInput = new Scanner(System.in);
 
         //Variables
@@ -18,88 +29,100 @@ public class UserAccountUI {
 
         //Initial menu asks user to create new account, log in with existing account, or exit
         while(!isDone){
-            System.out.println("Select 1 to create a new account, 2 for returning users, 3 to exit");
+            System.out.print("Welcome to CIS111 Bank. Please choose an option to continue:\n" +
+                             "1: Create a new account\n" +
+                             "2: Sign in\n" +
+                             "3: Exit\n");
+
+            //Variable to hold the option the user chooses
             userInput = kbInput.nextInt();
+
+            //Create new account if option 1 is chosen
             if(userInput == 1){
                 do{
-                    System.out.println("Please create a new account below." + "\n");
+                    System.out.println("Please create a new account below.");
                     System.out.println("Please enter an username that is not taken");
                     username = kbInput.next();
                     System.out.println("\n" + "Please enter a password that is not taken");
                     password = kbInput.next();
-                    useraccount.getUsers(username, password);
+                    useraccount.getUsers(username, password); //Checks existing user accounts to verify no duplicates
                     if(useraccount.checkNewUserAccount()){
                         System.out.println("Sorry that username or password was taken. Try again.");
                     }
                 }while(useraccount.checkNewUserAccount());
 
-                    useraccount.setUserAccount();
+                //When the Create Account process is successful, the account is set with the setUserAccount() method
+                useraccount.setUserAccount();
+
+                    // The second menu presents banking options to users and executes the appropriate method given the user input
                     while(!isDone){
                         System.out.println("Thank you for choosing us today, " + username.substring(0,1).toUpperCase() + username.substring(1));
-                        System.out.println("Select 1 to deposit, 2 to withdraw, 3 to check balance, 4 to print transaction history, 5 to quit");
+                        System.out.println("1: Deposit\n" +
+                                           "2: Withdraw\n" +
+                                           "3: Check Balance\n" +
+                                           "4: Show Transaction History\n" +
+                                           "5: Exit\n");
                         userInput = kbInput.nextInt();
                         switch (userInput){
-                            case 1:
+                            case 1: //Deposit
                                 useraccount.depositMethod();
                                 break;
-
-                            case 2:
+                            case 2: //Withdraw
                                 useraccount.withdrawMethod();
                                 break;
-
-                            case 3:
+                            case 3: //Check Balance
                                 useraccount.checkBalance();
                                 break;
-
-                            case 4:
+                            case 4: //Print Transaction History
                                 useraccount.transactionHistory();
                                 break;
-
-                            default:
+                            default: //Default case exits program
                                 System.out.println("Thank you for banking with us today" + "\n" + "\n");
                                 isDone = true;
                                 break;
                         }
                     }
-                    isDone = false; //lets the program continue
+                    isDone = false; //Lets the program continue
 
-            }else if(userInput == 2){
+            }else if(userInput == 2){ //If returning user, program will prompt user to log in using their credentials
                 System.out.println("Please enter your username");
                 username = kbInput.next(); //nextLine() was causing it to skip over, I don't exactly know why but I think it has something to do with nextInt();
                 System.out.println("\n" + "Please enter your password");
                 password = kbInput.next();
                 useraccount.getUsers(username, password);
+
+                //If checkUserAccount() method returns true, the user will be presented with the banking options menu
                 if(useraccount.checkUserAccount()){
                     System.out.println("\n" + "Welcome back, " + username.substring(0,1).toUpperCase() + username.substring(1));
                     while(!isDone){
-                        System.out.println("\n" + "Select 1 to deposit, 2 to withdraw, 3 to check balance, 4 to print transaction history, 5 to quit");
+                        System.out.println("1: Deposit\n" +
+                                "2: Withdraw\n" +
+                                "3: Check Balance\n" +
+                                "4: Show Transaction History\n" +
+                                "5: Exit\n");
                         userInput = kbInput.nextInt();
                         switch (userInput){
-                            case 1:
+                            case 1: //Deposit
                                 useraccount.depositMethod();
                                 break;
-
-                            case 2:
+                            case 2: //Withdraw
                                 useraccount.withdrawMethod();
                                 break;
-
-                            case 3:
+                            case 3: //Check Balance
                                 useraccount.checkBalance();
                                 break;
-
-                            case 4:
+                            case 4: //Transaction History
                                 useraccount.transactionHistory();
                                 break;
-
-                            default:
+                            default: //Default case exits the program
                                 System.out.println("Thank you for banking with us today" + "\n" + "\n");
                                 isDone = true;
                                 break;
                         }
                     }
                     isDone = false;
-                }else{
-                    System.out.println("One of the information above was entered incorrectly. Please try again" + "\n" + "\n");
+                }else{ //If the checkUserAccount() returns false, the user wil be prompted to retry the login
+                    System.out.println("Some of the information above was entered incorrectly. Please try again" + "\n");
                 }
             }else {
                 System.out.println("Thank you for banking with us today.");
